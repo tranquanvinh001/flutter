e6e6a86b932a12ebd50a8d9a868c0e7d2d95ba6a
VinhTranQuan <94186587+VinhTranQuan@users.noreply.github.com>
2023-12-16 17:04:51 +0700

    Update load resource
    Update load resource

--- ecommerce_app/lib/pages/home/main_food_page.dart
+++ ecommerce_app/lib/pages/home/main_food_page.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/controller/popular_product_controller.dart';
+import 'package:ecommerce_app/controller/recommended_product_controller.dart';
 import 'package:ecommerce_app/utils/colors.dart';
 import 'package:ecommerce_app/utils/dimensions.dart';
 import 'package:ecommerce_app/pages/home/food_page_body.dart';
@@ -14,59 +16,65 @@ class MainFoodPage extends StatefulWidget {
 }
 
 class _MainFoodPage extends State<MainFoodPage> {
+  Future<void> _loadResource() async {
+    await Get.find<PopularProductController>().getPopularProductList();
+    await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          //show the header
-          Container(
-            margin: EdgeInsets.only(
-                top: Dimensions.height55, bottom: Dimensions.height15),
-            padding: EdgeInsets.only(
-                left: Dimensions.width20, right: Dimensions.width20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    BigText(
-                      text: "Bangladesh",
-                      color: AppColors.mainColor,
-                    ),
-                    Row(
-                      children: [
-                        SmallText(
-                          text: "Narsingdi",
-                          color: Colors.black54,
-                        ),
-                        const Icon(Icons.arrow_drop_down_rounded)
-                      ],
-                    )
-                  ],
-                ),
-                Center(
-                  child: Container(
-                    width: Dimensions.height45,
-                    height: Dimensions.height45,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Dimensions.radius15),
-                      color: AppColors.mainColor,
-                    ),
-                    child: Icon(Icons.search,
-                        color: Colors.white, size: Dimensions.iconsize24),
+    return RefreshIndicator(
+        onRefresh: _loadResource,
+        child: Column(
+          children: [
+            //show the header
+            Container(
+              margin: EdgeInsets.only(
+                  top: Dimensions.height55, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(
+                  left: Dimensions.width20, right: Dimensions.width20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(
+                        text: "Bangladesh",
+                        color: AppColors.mainColor,
+                      ),
+                      Row(
+                        children: [
+                          SmallText(
+                            text: "Narsingdi",
+                            color: Colors.black54,
+                          ),
+                          const Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
                   ),
-                )
-              ],
+                  Center(
+                    child: Container(
+                      width: Dimensions.height45,
+                      height: Dimensions.height45,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radius15),
+                        color: AppColors.mainColor,
+                      ),
+                      child: Icon(Icons.search,
+                          color: Colors.white, size: Dimensions.iconsize24),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          //show the body
-          const Expanded(
-              child: SingleChildScrollView(
-            child: FoodPageBody(),
-          ))
-        ],
-      ),
-    );
+            //show the body
+            const Expanded(
+                child: SingleChildScrollView(
+              child: FoodPageBody(),
+            ))
+          ],
+        ));
   }
 }
